5a3906879127b7cd0222909d14b4943235cdd410
Ibrahim <inobani28@gmail.com>
2023-08-07 23:09:47 +0300

    Adding the Product Class

---
+++ Product.cs
@@ -0,0 +1,13 @@
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+}
